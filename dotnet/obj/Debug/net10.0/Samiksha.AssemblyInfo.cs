835256f053ee52f7b240a3fb14e147f7ab851c14
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-19 20:35:53 +0545

    Update AssemblyInformationalVersion in Samiksha.AssemblyInfo.cs

--- dotnet/obj/Debug/net10.0/Samiksha.AssemblyInfo.cs
+++ dotnet/obj/Debug/net10.0/Samiksha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59c3081ad390a767b166a4a22bc6069ee53c24f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8307fe09b8dec6e4405caa2661ff4b452a1a8670")]
 [assembly: System.Reflection.AssemblyProductAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

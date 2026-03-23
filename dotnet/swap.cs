8d333ae9658adee8a514720737fe6867f01262fd
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-23 07:31:47 +0545

    Add SwaptwoNumbers class with swapnumber method implementation

---
+++ dotnet/swap.cs
@@ -0,0 +1,12 @@
+namespace Swap;
+
+class SwaptwoNumbers
+{
+    public static void swapnumber()
+    {
+        int a = 5, b = 7, c;
+        c = b;
+        b = a;
+        Console.WriteLine("a:\t" + c + "\tb:\t" + b);
+    }
+}

bbec1749ca7238e0f71a65c6c2ac29b50a3c8b7c
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-23 08:04:37 +0545

    Refactor swap.cs to include implementation for swapping numbers without a third variable

--- dotnet/swap.cs
+++ dotnet/swap.cs
@@ -1,3 +1,5 @@
+//swap two numbers using third variable in C#
+
 namespace Swap;
 
 class SwaptwoNumbers
@@ -8,5 +10,18 @@ class SwaptwoNumbers
         c = b;
         b = a;
         Console.WriteLine("a:\t" + c + "\tb:\t" + b);
+    }
+}
+
+//swap two numbers without using third variable in C#
+class Swapwithoutthirdnum
+{
+    public static void SwaptwoNumberswithout()
+    {
+        int num1 = 10, num2 = 20;
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        Console.WriteLine("num1:\t" + num1 + "\tnum2:\t" + num2);
     }
 }

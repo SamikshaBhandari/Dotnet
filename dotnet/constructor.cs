9413d8b95cd77520f38d215ecf8011cd4423e33d
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-29 07:50:20 +0545

    Add ConstructorExample class with Calc method for basic arithmetic

---
+++ dotnet/constructor.cs
@@ -0,0 +1,11 @@
+namespace Constructor
+{
+    //default constructor
+    public class ConstructorExample
+    {
+        public int Calc(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

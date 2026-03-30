bb4e3f19d7b50f68b75835d732860915edc02895
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-30 07:00:25 +0545

    Add Overloadingmethod class with Add methods for two and three integers

---
+++ dotnet/overloading.cs
@@ -0,0 +1,17 @@
+namespace Overloading
+{
+    class Overloadingmethod
+    {
+        public void Add(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("Sum of two number:" + sum);
+        }
+
+        public void Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            Console.WriteLine("Sum of Three Number:" + sum);
+        }
+    }
+}

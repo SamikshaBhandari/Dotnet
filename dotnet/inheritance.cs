e72e5a1206d85a38b2b6714827106d44779fd5f6
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-06 07:48:01 +0545

    Fix typo in base constructor message and add Rectangle class extending Shape

--- dotnet/inheritance.cs
+++ dotnet/inheritance.cs
@@ -10,11 +10,18 @@ namespace Inheritance
     public Shape(float l,float b){
         this.length=l;
         this.breadth=b;
-        Console.WriteLine("class base cunstroctor");
+        Console.WriteLine("class base constructor");
     }
     public void display(){
         float area=length*breadth;
         Console.WriteLine($"Area is:{area}");
     }
+  }
+  public class Rectangle:Shape{
+    public Rectangle(float length,float breadth):base(length,breadth){
+        Console.WriteLine("class child constructor called");
+    }
+
+    
   }
 }

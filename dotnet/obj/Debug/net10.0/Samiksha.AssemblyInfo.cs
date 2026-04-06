0465602a5a3097a1a9f9346102797321ac7adb41
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-06 07:34:24 +0545

    Update binaries and refresh assembly information

--- dotnet/obj/Debug/net10.0/Samiksha.AssemblyInfo.cs
+++ dotnet/obj/Debug/net10.0/Samiksha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e29e4719131715ca46d5e78d6332fcfe234c631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5cb46b7681f1032c6cc988d22c21667029faf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Samiksha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
